cd8285e97ecb3772ccafefd438e084c35b3dcc69
Niyaa1786 <j4code1786@gmail.com>
2026-06-02 15:13:45 +0700

    feat: add Account entity and enums

---
+++ backend/UMS/UMS.Domain/Entities/Account.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Domain.Enums;
+
+namespace UMS.Domain.Entities
+{
+    public class Account
+    {
+        public Guid Id { get; private set; }
+        public string Username { get; private set; } = string.Empty;
+        public string PasswordHash { get; private set; } = string.Empty;
+        public Roles Role { get; private set; }
+        public bool IsActive { get; private set; }
+        public string? RefreshToken { get; private set; }
+        public DateTime RefreshTokenExpiry { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
+
+        private Account() { }
+
+        public Account(string username, string passwordHash, Roles role)
+        {
+            Id = Guid.NewGuid();
+            Username = username;
+            PasswordHash = passwordHash;
+            Role = role;
+            IsActive =  true;
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void UpdatePassword(string newPasswordHash)
+        {
+            PasswordHash = newPasswordHash;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void SetRefreshToken(string refreshToken, DateTime expiry)
+        {
+            RefreshToken = refreshToken;
+            RefreshTokenExpiry = expiry;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void RevokeRefreshToken()
+        {
+            RefreshToken = null;
+            RefreshTokenExpiry = DateTime.MinValue;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void Activate()
+        {
+            IsActive = true;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void Deactivate()
+        {
+            IsActive = false;
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
---
+++ backend/UMS/UMS.Domain/Enums/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UMS.Domain.Enums
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+}

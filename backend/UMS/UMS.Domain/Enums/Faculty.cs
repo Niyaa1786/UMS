0eb7f357f41e74da107690aa19ea99a7899dc132
Niyaa1786 <j4code1786@gmail.com>
2026-06-03 12:33:42 +0700

    feat: add Department and Faculty enums

---
+++ backend/UMS/UMS.Domain/Enums/Faculty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UMS.Domain.Enums
+{
+    public enum Faculty
+    {
+        Engineering,
+        Design,
+        Management,
+        Healthcare,
+        Languages
+    }
+}

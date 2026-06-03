3cd1dc84e86b61b9faf7fb12cd9de02b84c2f5a4
Niyaa1786 <j4code1786@gmail.com>
2026-06-03 14:16:04 +0700

    feat: add Staff entity

---
+++ backend/UMS/UMS.Domain/Entities/Staff.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Domain.Enums;
+
+namespace UMS.Domain.Entities
+{
+    public class Staff
+    {
+        public Guid Id { get; private set; }
+        public string FullName { get; private set; } = string.Empty;
+        public DateTime DateOfBirth { get; private set; }
+        public Gender Gender { get; private set; }
+        public string Email { get; private set; } = string.Empty;
+        public string Phone { get; private set; } = string.Empty;
+        public string Address { get; private set; } = string.Empty;
+        public Department Department { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+
+        private Staff() { }
+
+        public Staff(string fullName, string email, DateTime dateOfBirth, string phone, string address, Department department, Gender gender = Gender.Unknown)
+        {
+            Id = Guid.NewGuid();
+            FullName = fullName;
+            Email = email;
+            DateOfBirth = dateOfBirth;
+            Phone = phone;
+            Address = address;
+            Department = department;
+            Gender = gender;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public void UpdateProfile(Gender gender, string phone, string address)
+        {
+            Gender = gender;
+            Phone = phone;
+            Address = address;
+        }
+
+        public void UpdateDetails(string fullName, string email, string phone, string address, Department department, DateTime dateOfBirth, Gender gender)
+        {
+            FullName = fullName;
+            Email = email;
+            Phone = phone;
+            Address = address;
+            Department = department;
+            DateOfBirth = dateOfBirth;
+            Gender = gender;
+        }
+    }
+}

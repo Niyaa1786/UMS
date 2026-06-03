0e3ced9fc7645723ef8ad1c4a13447e9f07fb586
Niyaa1786 <j4code1786@gmail.com>
2026-06-03 22:01:11 +0700

    feat: implement PasswordHasher

---
+++ backend/UMS/UMS.Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,14 @@
+﻿using BCrypt.Net;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.Interfaces.Shared;
+
+namespace UMS.Infrastructure.Security
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password) => BCrypt.Net.BCrypt.HashPassword(password);
+        public bool VerifyPassword(string password, string passwordHash) => BCrypt.Net.BCrypt.Verify(password, passwordHash);
+    }
+}

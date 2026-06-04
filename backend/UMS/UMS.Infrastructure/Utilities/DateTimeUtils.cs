4cb81204cc9f69f09a323aac7904df0d216159e7
Niyaa1786 <j4code1786@gmail.com>
2026-06-04 14:42:04 +0700

    feat: add DateTimeUtils.cs

---
+++ backend/UMS/UMS.Infrastructure/Utilities/DateTimeUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UMS.Infrastructure.Utilities
+{
+    internal static class DateTimeUtils
+    {
+        public static string GetYearSuffix()
+        {
+            return DateTime.Now.Year.ToString("yy");
+        }
+    }
+}

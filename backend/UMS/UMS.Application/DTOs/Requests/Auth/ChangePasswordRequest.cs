f172a8ce54d091ed55f7b6fa160833dde0316759
Niyaa1786 <j4code1786@gmail.com>
2026-06-04 16:55:08 +0700

    feat: add Auth requests and responses DTOs

---
+++ backend/UMS/UMS.Application/DTOs/Requests/Auth/ChangePasswordRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UMS.Application.DTOs.Requests.Auth
+{
+    public class ChangePasswordRequest
+    {
+        public string OldPassword { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}
---
+++ backend/UMS/UMS.Application/DTOs/Responses/Auth/AuthResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Domain.Enums;
+
+namespace UMS.Application.DTOs.Responses.Auth
+{
+    public class AuthResponse
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public Roles Role { get; set; } 
+    }
+}

e6cff19839766ee5fd9540c7eff4acfac6ea9418
Niyaa1786 <j4code1786@gmail.com>
2026-06-04 21:02:49 +0700

    feat: add LoginRequestValidator

---
+++ backend/UMS/UMS.Application/Validator/Auth/LoginRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.DTOs.Requests.Auth;
+
+namespace UMS.Application.Validator.Auth
+{
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(x => x.Username)
+                .NotEmpty().WithMessage("Tài khoản đăng nhập không được để trống.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Mật khẩu không được để trống.");
+        }
+    }
+}

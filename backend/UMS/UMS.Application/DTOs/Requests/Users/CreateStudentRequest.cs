29b2b89b2098a81ccca3b869f3451528b290a1e6
Niyaa1786 <j4code1786@gmail.com>
2026-06-04 17:07:36 +0700

    feat: add Student requests and responses DTOs

---
+++ backend/UMS/UMS.Application/DTOs/Requests/Users/CreateStudentRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Domain.Enums;
+
+namespace UMS.Application.DTOs.Requests.Students
+{
+    public class CreateStudentRequest
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Major { get; set; } = string.Empty;
+        public Gender Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ backend/UMS/UMS.Application/DTOs/Responses/Users/StudentResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Domain.Enums;
+
+namespace UMS.Application.DTOs.Responses.Students
+{
+    public class StudentResponse
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Major { get; set; } = string.Empty;
+        public Gender Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public string StudentId { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+    }
+}

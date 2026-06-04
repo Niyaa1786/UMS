c455638b2faad1aa64176c7229d5189292ce27dd
Niyaa1786 <j4code1786@gmail.com>
2026-06-04 17:07:16 +0700

    feat: add Teacher requests and responses DTOs

---
+++ backend/UMS/UMS.Application/DTOs/Requests/Users/CreateTeacherRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Domain.Enums;
+
+namespace UMS.Application.DTOs.Requests.Users
+{
+    public class CreateTeacherRequest
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public Faculty Faculty { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ backend/UMS/UMS.Application/DTOs/Responses/Users/TeacherResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Domain.Enums;
+
+namespace UMS.Application.DTOs.Responses.Users
+{
+    public class TeacherResponse
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public Faculty Faculty { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public string TeacherId { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+    }
+}

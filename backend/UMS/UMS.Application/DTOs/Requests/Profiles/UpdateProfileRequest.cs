6defbfb12c541ffe24babd32a9f8db556e3ebbb2
Niyaa1786 <j4code1786@gmail.com>
2026-06-04 17:08:04 +0700

    feat: add Profile requests

---
+++ backend/UMS/UMS.Application/DTOs/Requests/Profiles/UpdateProfileRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Domain.Enums;
+
+namespace UMS.Application.DTOs.Requests.Profile
+{
+    public class UpdateProfileRequest
+    {
+        public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public Gender Gender { get; set; }
+    }
+}

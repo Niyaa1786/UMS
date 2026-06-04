7e6fe659d4c265084cb1c31e09a3e5b61bc8b33a
Niyaa1786 <j4code1786@gmail.com>
2026-06-04 21:03:43 +0700

    feat: add CreateTeacherRequestValidator

---
+++ backend/UMS/UMS.Application/Validator/Users/CreateTeacherRequestValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.DTOs.Requests.Users;
+
+namespace UMS.Application.Validator.Users
+{
+    public class CreateTeacherRequestValidator : AbstractValidator<CreateTeacherRequest>
+    {
+        public CreateTeacherRequestValidator()
+        {
+            RuleFor(x => x.FullName)
+                .NotEmpty().WithMessage("Họ và tên giảng viên không được để trống.")
+                .MaximumLength(100).WithMessage("Họ và tên không được vượt quá 100 ký tự.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email giảng viên không được để trống.")
+                .EmailAddress().WithMessage("Định dạng Email không hợp lệ.")
+                .MaximumLength(100).WithMessage("Email không được vượt quá 100 ký tự.");
+
+            RuleFor(x => x.Phone)
+                .NotEmpty().WithMessage("Số điện thoại không được để trống.")
+                .Matches(@"^\d{10}$").WithMessage("Số điện thoại phải chứa đúng 10 chữ số.");
+
+            RuleFor(x => x.Faculty)
+                .IsInEnum().WithMessage("Khoa giảng dạy được chọn không hợp lệ.");
+
+            RuleFor(x => x.Gender)
+                .IsInEnum().WithMessage("Giới tính được chọn không hợp lệ.");
+        }
+    }
+}

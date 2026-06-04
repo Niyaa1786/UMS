64f26d9348c49dc5a7399e8dc7d9a4ba2eadecfc
Niyaa1786 <j4code1786@gmail.com>
2026-06-04 21:15:43 +0700

    feat: add fluentvalidation and auto register validators

---
+++ backend/UMS/UMS.Application/AddApplicationDI.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.Validator.Profiles;
+
+namespace UMS.Application
+{
+    public static class AddApplicationDI
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssemblyContaining<UpdateProfileRequestValidator>();
+
+            return services;
+        }
+    }
+}

95055ab92bce076a8bee8198c769e42f56e0e169
Niyaa1786 <j4code1786@gmail.com>
2026-06-05 15:01:26 +0700

    feat: add LogoutUseCase

---
+++ backend/UMS/UMS.Application/UseCases/Auth/LogoutUseCase.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.Interfaces.Shared;
+
+namespace UMS.Application.UseCases.Auth
+{
+    internal class LogoutUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public LogoutUseCase(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        public async Task<bool> ExecuteAsync(Guid id, CancellationToken ct = default)
+        {
+            var user = await _unitOfWork.Accounts.GetByIdAsync(id, ct);
+            if (user is null)
+                throw new ValidationException(new[] { new ValidationFailure("Id", "Account not found") });
+
+            user.RevokeRefreshToken();
+            await _unitOfWork.SaveChangesAsync(ct);
+
+            return true;
+        }
+    }
+}

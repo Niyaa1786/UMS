64647f48473f4582d43d655d4aba542126cd0b82
Niyaa1786 <j4code1786@gmail.com>
2026-06-05 15:01:41 +0700

    feat: add RefreshTokenUseCase

---
+++ backend/UMS/UMS.Application/UseCases/Auth/RefreshTokenUseCase.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using UMS.Application.DTOs.Requests.Auth;
+using UMS.Application.DTOs.Responses.Auth;
+using UMS.Application.Interfaces.Common;
+using UMS.Application.Interfaces.Shared;
+using UMS.Application.Mappers;
+using UMS.Domain.Enums;
+
+namespace UMS.Application.UseCases.Auth
+{
+    internal class RefreshTokenUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IValidator<RefreshTokenRequest> _validator;
+        private readonly ITokenGenerator _tokenGenerator;
+
+        public RefreshTokenUseCase(IUnitOfWork unitOfWork, IValidator<RefreshTokenRequest> validator, ITokenGenerator tokenGenerator)
+        {
+            _unitOfWork = unitOfWork;
+            _validator = validator;
+            _tokenGenerator = tokenGenerator;
+        }
+
+        public async Task<AuthResponse> ExecuteAsync(RefreshTokenRequest request, CancellationToken ct = default)
+        {
+            _validator.ValidateAndThrow(request);
+
+            var account = await _unitOfWork.Accounts.GetByRefreshTokenAsync(request.RefreshToken, ct);
+
+            if(account is null || account.RefreshTokenExpiry < DateTime.UtcNow)
+                throw new ValidationException(new[] {new ValidationFailure("RefreshToken", "Invalid refresh token.")});
+
+            var tokenResult = _tokenGenerator.GenerateToken(account);
+
+            account.SetRefreshToken(tokenResult.RefreshToken, tokenResult.RefreshTokenExpiration);
+            await _unitOfWork.SaveChangesAsync(ct);
+
+            return AuthMapper.ToResponse(account, tokenResult);
+        }
+    }
+}

16697a8b9790e095742c51eac1f8391297528b12
Niyaa1786 <j4code1786@gmail.com>
2026-06-05 14:07:08 +0700

    add: RefreshTokenRequestValidator and Dto

---
+++ backend/UMS/UMS.Application/DTOs/Requests/Auth/RefreshTokenRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UMS.Application.DTOs.Requests.Auth
+{
+    public class RefreshTokenRequest
+    {
+        public string RefreshToken { get; set; } = string.Empty;
+    }
+}
---
+++ backend/UMS/UMS.Application/Validator/Auth/RefreshTokenRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.DTOs.Requests.Auth;
+
+namespace UMS.Application.Validator.Auth
+{
+    public class RefreshTokenRequestValidator : AbstractValidator<RefreshTokenRequest>
+    {
+        public RefreshTokenRequestValidator()
+        {
+            RuleFor(r => r.RefreshToken).NotEmpty().WithMessage("Refresh token không được để trống.");
+        }
+    }
+}

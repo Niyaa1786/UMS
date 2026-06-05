a2ffd1ab99b39373a9f2ce0091afeda6861aa963
Niyaa1786 <j4code1786@gmail.com>
2026-06-05 16:41:19 +0700

    feat(json): enable camelCase, case-insensitive, and string enum serialization

--- backend/UMS/UMS.Api/Program.cs
+++ backend/UMS/UMS.Api/Program.cs
@@ -1,6 +1,8 @@
 using Scalar.AspNetCore;
 using Serilog;
 using Serilog.Events;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
@@ -12,7 +14,12 @@ try
     // Add services to the container.
     builder.Services.AddSerilog();
 
-    builder.Services.AddControllers();
+    builder.Services.AddControllers().AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
     // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
     builder.Services.AddOpenApi();
 

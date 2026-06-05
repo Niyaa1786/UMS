52fcdd8df5871fe1bd401a89f932203ce53794a1
Niyaa1786 <j4code1786@gmail.com>
2026-06-05 15:01:14 +0700

    feat: add LoginUseCase

---
+++ backend/UMS/UMS.Application/UseCases/Auth/LoginUseCase.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.DTOs.Requests.Auth;
+using UMS.Application.DTOs.Responses.Auth;
+using UMS.Application.Interfaces.Common;
+using UMS.Application.Interfaces.Shared;
+using UMS.Application.Mappers;
+using UMS.Domain.Enums;
+
+namespace UMS.Application.UseCases.Auth
+{
+    internal class LoginUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IValidator<LoginRequest> _validator;
+        private readonly ITokenGenerator _tokenGenerator;
+        private readonly IPasswordHasher _passwordHasher;
+
+        public LoginUseCase(IUnitOfWork unitOfWork, IValidator<LoginRequest> validator, ITokenGenerator tokenGenerator, IPasswordHasher passwordHasher)
+        {
+            _unitOfWork = unitOfWork;
+            _validator = validator;
+            _tokenGenerator = tokenGenerator;
+            _passwordHasher = passwordHasher;
+        }
+
+        public async Task<AuthResponse> ExecuteAsync(LoginRequest request, CancellationToken ct = default)
+        {
+            _validator.ValidateAndThrow(request);
+
+            var account = await _unitOfWork.Accounts.GetByUsernameAsync(request.Username, ct);
+
+            if(account is null)
+                throw new ValidationException(new[] { new ValidationFailure("Username", "Invalid username or password.") });
+
+            var isValidPassword = _passwordHasher.VerifyPassword(request.Password, account.PasswordHash);
+
+            if(!isValidPassword)
+                throw new ValidationException(new[] { new ValidationFailure("Password", "Invalid username or password.") });
+
+            var tokenResult = _tokenGenerator.GenerateToken(account);
+
+            return AuthMapper.ToResponse(account, tokenResult);
+        }
+    }
+}

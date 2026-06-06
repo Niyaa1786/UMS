61990cb048084edb8a2e6f113caefffd05137f44
Niyaa1786 <j4code1786@gmail.com>
2026-06-06 14:18:28 +0700

    feat: add AuthController

---
+++ backend/UMS/UMS.Api/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UMS.Api.Reponses;
+using UMS.Application.DTOs.Requests.Auth;
+using UMS.Application.DTOs.Responses.Auth;
+using UMS.Application.Facades;
+
+namespace UMS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly IAuthFacade _authFacade;
+        public AuthController(IAuthFacade authFacade)
+        {
+            _authFacade = authFacade;
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginRequest request, CancellationToken ct)
+        {
+            var result = await _authFacade.LoginAsync(request, ct);
+            var res = ApiResponse<AuthResponse>.Success(result, "Login sucessfully");
+
+            return Ok(res);
+        }
+
+        [HttpPost("Logout")]
+        public async Task<IActionResult> Logout(Guid id, CancellationToken ct)
+        {
+            await _authFacade.LogoutAsync(id, ct);
+            var res = ApiResponse<object>.Success(null!, "Logout sucessfully");
+
+            return Ok(res);
+        }
+
+        [HttpPost("RefreshToken")]
+        public async Task<IActionResult> RefreshToken(RefreshTokenRequest request, CancellationToken ct)
+        {
+            var result = await _authFacade.RefreshTokenAsync(request, ct);
+            var res = ApiResponse<AuthResponse>.Success(result, "Token refreshed");
+
+            return Ok(res);
+        }
+
+    }
+}

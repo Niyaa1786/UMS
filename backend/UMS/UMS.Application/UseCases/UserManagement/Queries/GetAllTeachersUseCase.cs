c0ade4b8f1e34856ad3de51c7ef62826a405c861
Niyaa1786 <j4code1786@gmail.com>
2026-06-07 18:23:06 +0700

    feat: add GetAllTeachersUseCase

---
+++ backend/UMS/UMS.Application/UseCases/UserManagement/Queries/GetAllTeachersUseCase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.DTOs.Responses.Students;
+using UMS.Application.DTOs.Responses.Users;
+using UMS.Application.Interfaces.Shared;
+using UMS.Application.Mappers;
+
+namespace UMS.Application.UseCases.UserManagement.Queries
+{
+    internal class GetAllTeachersUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetAllTeachersUseCase(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<TeacherResponse>> ExecuteAsync(CancellationToken ct = default)
+        {
+            var teachers = await _unitOfWork.Teachers.GetAllAsync(ct);
+
+            return teachers.Select(UserMapper.ToResponse);
+        }
+    }
+}

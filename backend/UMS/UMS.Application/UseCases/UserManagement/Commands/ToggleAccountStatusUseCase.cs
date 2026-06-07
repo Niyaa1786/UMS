89f064f088165a6e4bbed8c1866484c36fc49dd7
Niyaa1786 <j4code1786@gmail.com>
2026-06-07 18:11:31 +0700

    feat: add ToggleAccountStatusUseCase

---
+++ backend/UMS/UMS.Application/UseCases/UserManagement/Commands/ToggleAccountStatusUseCase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.Exceptions;
+using UMS.Application.Interfaces.Shared;
+using UMS.Domain.Entities;
+
+namespace UMS.Application.UseCases.UserManagement.Commands
+{
+    internal class ToggleAccountStatusUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ToggleAccountStatusUseCase(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task ExecuteAsync(Guid accountId, bool isActive, CancellationToken ct = default)
+        {
+            var account = await _unitOfWork.Accounts.GetByIdAsync(accountId, ct);
+
+            if (account == null)
+                throw new NotFoundException($"Account with id {accountId} not found.");
+
+            if (isActive)
+            {
+                account.Activate();
+            }
+            else
+            {
+                account.Deactivate();
+            }
+
+            await _unitOfWork.SaveChangesAsync(ct);
+        }
+    }
+}

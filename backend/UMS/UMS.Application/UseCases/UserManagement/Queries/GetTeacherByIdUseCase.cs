5232dac5fc49c18324f17142bf475fec051716ac
Niyaa1786 <j4code1786@gmail.com>
2026-06-07 18:23:40 +0700

    feat: add GetTeacherByIdUseCase

---
+++ backend/UMS/UMS.Application/UseCases/UserManagement/Queries/GetTeacherByIdUseCase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.DTOs.Responses.Students;
+using UMS.Application.DTOs.Responses.Users;
+using UMS.Application.Exceptions;
+using UMS.Application.Interfaces.Shared;
+using UMS.Application.Mappers;
+
+namespace UMS.Application.UseCases.UserManagement.Queries
+{
+    internal class GetTeacherByIdUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetTeacherByIdUseCase(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<TeacherResponse> ExecuteAsync(Guid teacherId, CancellationToken ct)
+        {
+            var teacher = await _unitOfWork.Teachers.GetByIdAsync(teacherId, ct);
+
+            if (teacher is null)
+                throw new NotFoundException($"Staff with id {teacherId} not found.");
+
+            return UserMapper.ToResponse(teacher);
+        }
+    }
+}

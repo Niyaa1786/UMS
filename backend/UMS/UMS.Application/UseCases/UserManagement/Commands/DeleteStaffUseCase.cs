defe55d4bcddcd657ab249ca353db4c2b566744a
Niyaa1786 <j4code1786@gmail.com>
2026-06-07 18:07:47 +0700

    feat: add DeleteStaffUseCase

---
+++ backend/UMS/UMS.Application/UseCases/UserManagement/Commands/DeleteStaffUseCase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.Exceptions;
+using UMS.Application.Interfaces.Shared;
+
+namespace UMS.Application.UseCases.UserManagement.Commands
+{
+    internal class DeleteStaffUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteStaffUseCase(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> ExecuteAsync(Guid staffId, CancellationToken ct = default)
+        {
+            var staff = await _unitOfWork.Staffs.GetByIdAsync(staffId, ct);
+            if (staff is null)
+                throw new NotFoundException($"Staff with id {staffId} not found.");
+
+            var account = await _unitOfWork.Accounts.GetByIdAsync(staff.AccountId, ct);
+            if (account is null)
+                throw new NotFoundException($"Account with id {staff.AccountId} for Staff {staffId} not found..");
+
+            _unitOfWork.Staffs.Remove(staff);
+            _unitOfWork.Accounts.Remove(account);
+            await _unitOfWork.SaveChangesAsync(ct);
+
+            return true;
+        }
+    }
+}

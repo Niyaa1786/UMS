d807ed042fc7e87156364088ee889168ae0dfe7b
Niyaa1786 <j4code1786@gmail.com>
2026-06-07 18:07:31 +0700

    feat: add CreateTeacherUseCase

---
+++ backend/UMS/UMS.Application/UseCases/UserManagement/Commands/CreateTeacherUseCase.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.DTOs.Requests.Users;
+using UMS.Application.DTOs.Responses.Users;
+using UMS.Application.Interfaces.Common;
+using UMS.Application.Interfaces.Shared;
+using UMS.Application.Mappers;
+using UMS.Domain.Entities;
+using UMS.Domain.Enums;
+
+namespace UMS.Application.UseCases.UserManagement.Commands
+{
+    internal class CreateTeacherUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IIdentityGenerator _identityGenerator;
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly IValidator<CreateTeacherRequest> _validator;
+
+        public CreateTeacherUseCase(IUnitOfWork unitOfWork, IIdentityGenerator identityGenerator, IPasswordHasher passwordHasher, IValidator<CreateTeacherRequest> validator)
+        {
+            _unitOfWork = unitOfWork;
+            _identityGenerator = identityGenerator;
+            _passwordHasher = passwordHasher;
+            _validator = validator;
+        }
+
+        public async Task<TeacherResponse> ExecuteAsync(CreateTeacherRequest request, CancellationToken ct = default)
+        {
+            _validator.ValidateAndThrow(request);
+
+            var teacherCode = await _identityGenerator.GenerateTeacherIdAsync(ct);
+            var passwordHash = _passwordHasher.HashPassword($"GV@{teacherCode}");
+
+            var account = new Account(teacherCode, passwordHash, Roles.Staff);
+            var teacher = UserMapper.ToEntity(request, account.Id);
+
+            _unitOfWork.Accounts.Add(account);
+            _unitOfWork.Teachers.Add(teacher);
+            await _unitOfWork.SaveChangesAsync(ct);
+
+            var savedTeacher = await _unitOfWork.Teachers.GetByIdAsync(teacher.Id, ct);
+            return UserMapper.ToResponse(savedTeacher!);
+        }
+    }
+}

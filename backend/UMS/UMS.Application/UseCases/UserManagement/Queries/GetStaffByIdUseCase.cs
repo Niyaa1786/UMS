0368e44fcfbba364167cfd7e22eaeb1d9f115035
Niyaa1786 <j4code1786@gmail.com>
2026-06-07 18:23:20 +0700

    feat: add GetStaffByIdUseCase

---
+++ backend/UMS/UMS.Application/UseCases/UserManagement/Queries/GetStaffByIdUseCase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+using UMS.Application.DTOs.Responses.Users;
+using UMS.Application.Exceptions;
+using UMS.Application.Interfaces.Shared;
+using UMS.Application.Mappers;
+using UMS.Domain.Entities;
+
+namespace UMS.Application.UseCases.UserManagement.Queries
+{
+    internal class GetStaffByIdUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetStaffByIdUseCase(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<StaffResponse> ExecuteAsync(Guid staffId, CancellationToken ct)
+        {
+            var staff = await _unitOfWork.Staffs.GetByIdAsync(staffId, ct);
+
+            if(staff is null)
+                throw new NotFoundException($"Staff with id {staffId} not found.");
+
+            return UserMapper.ToResponse(staff);
+        }
+    }
+}

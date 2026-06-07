4216346c6117066fa167ce3cda455c31fc4ff563
Niyaa1786 <j4code1786@gmail.com>
2026-06-07 18:09:02 +0700

    feat: add ToggleAccountStatusUseCase

---
+++ backend/UMS/UMS.Application/UseCases/UserManagement/Queries/GetAllStaffsUseCase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.DTOs.Responses.Students;
+using UMS.Application.DTOs.Responses.Users;
+using UMS.Application.Interfaces.Shared;
+using UMS.Application.Mappers;
+
+namespace UMS.Application.UseCases.UserManagement.Queries
+{
+    internal class GetAllStaffsUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetAllStaffsUseCase(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<StaffResponse>> ExecuteAsync(CancellationToken ct = default)
+        {
+            var staffs = await _unitOfWork.Staffs.GetAllAsync(ct);
+
+            return staffs.Select(UserMapper.ToResponse);
+        }
+    }
+}

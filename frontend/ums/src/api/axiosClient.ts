2f21887274d1a3556ead3fb98dd85bf8b763ee55
Niyaa1786 <j4code1786@gmail.com>
2026-06-08 21:27:47 +0700

    chore(fe): update axiosClient

--- frontend/ums/src/api/axiosClient.ts
+++ frontend/ums/src/api/axiosClient.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { type AxiosResponse } from 'axios'
 
 const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -14,8 +14,8 @@ axiosClient.interceptors.request.use((config) => {
 })
 
 axiosClient.interceptors.response.use(
-  (config) => {
-    return config.data
+  (response) => {
+    return response
   },
   async (error) => {
     const originalRequest = error.config

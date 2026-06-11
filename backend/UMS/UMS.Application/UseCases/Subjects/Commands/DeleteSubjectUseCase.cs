60cec92ae52f227e8bbb8b1855f3e8e22f4fa82b
Niyaa1786 <j4code1786@gmail.com>
2026-06-11 21:32:36 +0700

    feat: add DeleteSubjectUseCase

---
+++ backend/UMS/UMS.Application/UseCases/Subjects/Commands/DeleteSubjectUseCase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.Exceptions;
+using UMS.Application.Interfaces.Shared;
+
+namespace UMS.Application.UseCases.Subjects.Commands
+{
+    internal class DeleteSubjectUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteSubjectUseCase(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        public async Task<bool> ExecuteAsync(Guid id, CancellationToken ct = default)
+        {
+            var subject = await _unitOfWork.Subjects.GetByIdAsync(id, ct);
+            if (subject is null)
+                throw new NotFoundException($"Không tìm thấy môn học với id {id}.");
+
+            _unitOfWork.Subjects.Remove(subject);
+            await _unitOfWork.SaveChangesAsync(ct);
+            return true;
+        }
+
+    }
+}

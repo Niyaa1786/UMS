97bcd180bccff69e75bcb9ff43322d2e66cb9447
Niyaa1786 <j4code1786@gmail.com>
2026-06-11 21:32:43 +0700

    feat: add GetAllSubjectsUseCase

---
+++ backend/UMS/UMS.Application/UseCases/Subjects/Queries/GetAllSubjectsUseCase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.DTOs.Responses.Subject;
+using UMS.Application.Interfaces.Shared;
+using UMS.Application.Mappers;
+
+namespace UMS.Application.UseCases.Subjects.Queries
+{
+    internal class GetAllSubjectsUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetAllSubjectsUseCase(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        public async Task<IEnumerable<SubjectResponse>> ExecuteAsync(CancellationToken ct = default)
+        {
+            var subjects = await _unitOfWork.Subjects.GetAllAsync(ct);
+            return subjects.Select(SubjectMapper.ToResponse);
+        }
+
+    }
+}

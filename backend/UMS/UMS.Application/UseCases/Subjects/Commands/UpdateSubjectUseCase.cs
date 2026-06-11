b20b074c857999ec3076b2cf55f543e2b1831700
Niyaa1786 <j4code1786@gmail.com>
2026-06-11 21:32:26 +0700

    feat: add UpdateSubjectUseCase

---
+++ backend/UMS/UMS.Application/UseCases/Subjects/Commands/UpdateSubjectUseCase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UMS.Application.UseCases.Subjects.Commands
+{
+    internal class UpdateSubjectUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IValidator<UpdateSubjectRequest> _validator;
+
+        public UpdateSubjectUseCase(IUnitOfWork unitOfWork, IValidator<UpdateSubjectRequest> validator)
+        {
+            _unitOfWork = unitOfWork;
+            _validator = validator;
+        }
+
+        public async Task<SubjectResponse> ExecuteAsync(Guid id, UpdateSubjectRequest request, CancellationToken ct = default)
+        {
+            _validator.ValidateAndThrow(request);
+
+            var subject = await _unitOfWork.Subjects.GetByIdAsync(id, ct);
+            if (subject is null)
+                throw new NotFoundException($"Không tìm thấy môn học với id {id}.");
+
+            SubjectMapper.ApplyUpdate(request, subject);
+            await _unitOfWork.SaveChangesAsync(ct);
+
+            return SubjectMapper.ToResponse(subject);
+        }
+
+    }
+}

0d4658c43146b733c791de323667906be97c2ad1
Niyaa1786 <j4code1786@gmail.com>
2026-06-11 16:51:03 +0700

    feat: add dtos for Class

---
+++ backend/UMS/UMS.Application/DTOs/Requests/Class/CreateClassRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UMS.Application.DTOs.Requests.Class
+{
+    public class CreateClassRequest
+    {
+        public string Code { get; set; } = string.Empty;
+        public Guid SubjectId { get; set; }
+        public Guid TeacherId { get; set; }
+        public string SchoolYear { get; set; } = string.Empty;
+        public int Semester { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int MaxStudents { get; set; }
+    }
+}
---
+++ backend/UMS/UMS.Application/DTOs/Responses/Class/ClassResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Domain.Enums;
+
+namespace UMS.Application.DTOs.Responses.Class
+{
+    public class ClassResponse
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public Guid SubjectId { get; set; }
+        public string SubjectName { get; set; } = string.Empty;
+        public Guid TeacherId { get; set; }
+        public string TeacherName { get; set; } = string.Empty;
+        public string SchoolYear { get; set; } = string.Empty;
+        public int Semester { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int MaxStudents { get; set; }
+        public Status Status { get; set; }
+    }
+}

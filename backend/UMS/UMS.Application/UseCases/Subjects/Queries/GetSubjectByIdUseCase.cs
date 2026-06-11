f211a48d0830d57ec257ef12043f8e46f1983a88
Niyaa1786 <j4code1786@gmail.com>
2026-06-11 21:32:56 +0700

    feat: add GetSubjectByIdUseCase

---
+++ backend/UMS/UMS.Application/UseCases/Subjects/Queries/GetSubjectByIdUseCase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.DTOs.Responses.Subject;
+using UMS.Application.Exceptions;
+using UMS.Application.Interfaces.Shared;
+using UMS.Application.Mappers;
+
+namespace UMS.Application.UseCases.Subjects.Queries
+{
+    internal class GetSubjectByIdUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetSubjectByIdUseCase(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        public async Task<SubjectResponse> ExecuteAsync(Guid id, CancellationToken ct = default)
+        {
+            var subject = await _unitOfWork.Subjects.GetByIdAsync(id, ct);
+            if (subject is null)
+                throw new NotFoundException($"Không tìm thấy môn học với id {id}.");
+
+            return SubjectMapper.ToResponse(subject);
+        }
+
+    }
+}

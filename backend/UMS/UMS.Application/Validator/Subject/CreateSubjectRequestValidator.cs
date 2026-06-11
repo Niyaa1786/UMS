f945005bd203fcfa7659fd29cebd698d98c2cba3
Niyaa1786 <j4code1786@gmail.com>
2026-06-11 19:07:21 +0700

    feat: add Subject validator

---
+++ backend/UMS/UMS.Application/Validator/Subject/CreateSubjectRequestValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.DTOs.Requests.Subjects;
+
+namespace UMS.Application.Validator.Subject
+{
+    public class CreateSubjectRequestValidator : AbstractValidator<CreateSubjectRequest>
+    {
+        public CreateSubjectRequestValidator()
+        {
+            RuleFor(x => x.Code)
+                .NotEmpty().WithMessage("Mã môn học không được để trống.")
+                .MaximumLength(20).WithMessage("Mã môn học tối đa 20 ký tự.");
+
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Tên môn học không được để trống.")
+                .MaximumLength(100).WithMessage("Tên môn học tối đa 100 ký tự.");
+
+            RuleFor(x => x.Credits)
+                .GreaterThan(0).WithMessage("Số tín chỉ phải lớn hơn 0.");
+        }
+    }
+
+}
---
+++ backend/UMS/UMS.Application/Validator/Subject/UpdateSubjectRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.DTOs.Requests.Subjects;
+
+namespace UMS.Application.Validator.Subject
+{
+    public class UpdateSubjectRequestValidator : AbstractValidator<UpdateSubjectRequest>
+    {
+        public UpdateSubjectRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Tên môn học không được để trống.")
+                .MaximumLength(100).WithMessage("Tên môn học tối đa 100 ký tự.");
+
+            RuleFor(x => x.Credits)
+                .GreaterThan(0).WithMessage("Số tín chỉ phải lớn hơn 0.");
+        }
+    }
+
+}

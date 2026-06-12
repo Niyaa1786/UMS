24ae1c25075f14a77988f0b5b99cd2183c101c51
Niyaa1786 <j4code1786@gmail.com>
2026-06-12 11:21:16 +0700

    feat: add GetClassSchedulesByClassIdUseCase

---
+++ backend/UMS/UMS.Application/UseCases/Class/Queries/GetClassSchedulesByClassIdUseCase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.DTOs.Responses.Class;
+using UMS.Application.Interfaces.Shared;
+using UMS.Application.Mappers;
+
+namespace UMS.Application.UseCases.Class.Queries
+{
+    internal class GetClassSchedulesByClassIdUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public GetClassSchedulesByClassIdUseCase(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        public async Task<IEnumerable<ClassScheduleResponse>> ExecuteAsync(Guid classId, CancellationToken ct = default)
+        {
+            var schedules = await _unitOfWork.ClassSchedules.GetByClassIdAsync(classId, ct);
+            return schedules.Select(ClassScheduleMapper.ToResponse);
+        }
+    }
+}

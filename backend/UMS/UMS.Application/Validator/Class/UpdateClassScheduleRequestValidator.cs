9587e6b2c143c523234ac8427d8119da2e4429e3
Niyaa1786 <j4code1786@gmail.com>
2026-06-12 12:51:06 +0700

    feat: add validation rules for UpdateClassScheduleRequest

--- backend/UMS/UMS.Application/Validator/Class/UpdateClassScheduleRequestValidator.cs
+++ backend/UMS/UMS.Application/Validator/Class/UpdateClassScheduleRequestValidator.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using UMS.Application.DTOs.Requests.Class;
 
 namespace UMS.Application.Validator.Class
 {
-    internal class UpdateClassScheduleRequestValidator
+    public class UpdateClassScheduleRequestValidator : AbstractValidator<UpdateClassScheduleRequest>
     {
+        public UpdateClassScheduleRequestValidator()
+        {
+            RuleFor(x => x.DayOfWeek)
+                .IsInEnum().WithMessage("Ngày trong tuần không hợp lệ.");
+
+            RuleFor(x => x.StartTime)
+                .LessThan(x => x.EndTime).WithMessage("Thời gian bắt đầu phải trước thời gian kết thúc.");
+
+            RuleFor(x => x.Room)
+                .NotEmpty().WithMessage("Phòng học không được để trống.")
+                .MaximumLength(50).WithMessage("Phòng học tối đa 50 ký tự.");
+        }
     }
+
 }

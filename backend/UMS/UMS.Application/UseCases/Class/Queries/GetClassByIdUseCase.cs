489d050d5ce20edb73491ce5c54715e4ad039859
Niyaa1786 <j4code1786@gmail.com>
2026-06-12 10:59:23 +0700

    feat: add GetClassByIdUseCase

---
+++ backend/UMS/UMS.Application/UseCases/Class/Queries/GetClassByIdUseCase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.DTOs.Responses.Class;
+using UMS.Application.Exceptions;
+using UMS.Application.Interfaces.Shared;
+using UMS.Application.Mappers;
+
+namespace UMS.Application.UseCases.Class.Queries
+{
+    internal class GetClassByIdUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public GetClassByIdUseCase(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        public async Task<ClassResponse> ExecuteAsync(Guid id, CancellationToken ct = default)
+        {
+            var classEntity = await _unitOfWork.Classes.GetByIdAsync(id, ct);
+            if (classEntity is null)
+                throw new NotFoundException($"Không tìm thấy lớp với id {id}.");
+
+            var subject = await _unitOfWork.Subjects.GetByIdAsync(classEntity.SubjectId, ct);
+            var teacher = await _unitOfWork.Teachers.GetByIdAsync(classEntity.TeacherId, ct);
+            return ClassMapper.ToResponse(classEntity, subject!.Name, teacher!.FullName);
+        }
+    }
+}

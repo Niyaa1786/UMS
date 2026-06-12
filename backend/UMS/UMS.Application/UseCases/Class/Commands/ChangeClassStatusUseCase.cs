70361f55b15bd45565704cf08e4754e45ac82e30
Niyaa1786 <j4code1786@gmail.com>
2026-06-12 10:50:38 +0700

    feat: add ChangeClassStatusUseCase

---
+++ backend/UMS/UMS.Application/UseCases/Class/Commands/ChangeClassStatusUseCase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.Exceptions;
+using UMS.Application.Interfaces.Shared;
+using UMS.Domain.Enums;
+
+namespace UMS.Application.UseCases.Class.Commands
+{
+    internal class ChangeClassStatusUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ChangeClassStatusUseCase(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        public async Task<bool> ExecuteAsync(Guid id, bool isActive, CancellationToken ct = default)
+        {
+            var classEntity = await _unitOfWork.Classes.GetByIdAsync(id, ct);
+            if (classEntity is null)
+                throw new NotFoundException($"Không tìm thấy lớp với id {id}.");
+
+            classEntity.ChangeStatus(isActive ? Status.Active : Status.Closed);
+            await _unitOfWork.SaveChangesAsync(ct);
+            return true;
+        }
+
+    }
+}

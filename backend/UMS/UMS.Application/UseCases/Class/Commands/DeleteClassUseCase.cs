7cdf08042c529b95f20a9e865c02845acb081253
Niyaa1786 <j4code1786@gmail.com>
2026-06-12 10:50:24 +0700

    feat: add DeleteClassUseCase

---
+++ backend/UMS/UMS.Application/UseCases/Class/Commands/DeleteClassUseCase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.Exceptions;
+using UMS.Application.Interfaces.Shared;
+
+namespace UMS.Application.UseCases.Class.Commands
+{
+    internal class DeleteClassUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public DeleteClassUseCase(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        public async Task<bool> ExecuteAsync(Guid id, CancellationToken ct = default)
+        {
+            var classEntity = await _unitOfWork.Classes.GetByIdAsync(id, ct);
+            if (classEntity is null)
+                throw new NotFoundException($"Không tìm thấy lớp với id {id}.");
+
+            _unitOfWork.Classes.Remove(classEntity);
+            await _unitOfWork.SaveChangesAsync(ct);
+            return true;
+        }
+    }
+}

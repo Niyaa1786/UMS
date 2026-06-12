a8d1c147b055d42480e5020ce9ad9f91833b6278
Niyaa1786 <j4code1786@gmail.com>
2026-06-12 10:51:18 +0700

    feat: add GetAllClassesUseCase

---
+++ backend/UMS/UMS.Application/UseCases/Class/Queries/GetAllClassesUseCase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.DTOs.Responses.Class;
+using UMS.Application.Interfaces.Shared;
+using UMS.Application.Mappers;
+
+namespace UMS.Application.UseCases.Class.Queries
+{
+    internal class GetAllClassesUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public GetAllClassesUseCase(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        public async Task<IEnumerable<ClassResponse>> ExecuteAsync(CancellationToken ct = default)
+        {
+            var classes = await _unitOfWork.Classes.GetAllAsync(ct);
+            return classes.Select(ClassMapper.ToResponse);
+        }
+    }
+}

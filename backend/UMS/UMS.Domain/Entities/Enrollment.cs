67a8dc1d66f20ac012880d1aae8399a2b0d51796
Niyaa1786 <j4code1786@gmail.com>
2026-06-16 13:48:03 +0700

    feat: ddd Enrollment domain entity

---
+++ backend/UMS/UMS.Domain/Entities/Enrollment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UMS.Domain.Entities
+{
+    public class Enrollment
+    {
+        public Guid Id { get; private set; }
+        public Guid ClassId { get; private set; }
+        public Guid StudentId { get; private set; }
+        public DateTime EnrolledAt { get; private set; }
+        public string Status { get; private set; }
+
+        public Class? Class { get; private set; }
+        public Student? Student { get; private set; }
+
+        private Enrollment() { }
+
+        public Enrollment(Guid classId, Guid studentId)
+        {
+            Id = Guid.NewGuid();
+            ClassId = classId;
+            StudentId = studentId;
+            EnrolledAt = DateTime.UtcNow;
+            Status = "Active";
+        }
+
+        public void Drop()
+        {
+            Status = "Dropped";
+        }
+    }
+
+}

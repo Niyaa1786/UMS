377d214832932bfff2afcddbce166e3e9b85ca37
Niyaa1786 <j4code1786@gmail.com>
2026-06-19 17:38:38 +0700

    feat: add GetEnrollmentsByStudentUseCase

---
+++ backend/UMS/UMS.Application/UseCases/ClassManagement/Queries/GetEnrollmentsByStudentUseCase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMS.Application.DTOs.Responses.Class;
+using UMS.Application.Interfaces.Shared;
+using UMS.Application.Mappers;
+
+namespace UMS.Application.UseCases.ClassManagement.Queries
+{
+    internal class GetEnrollmentsByStudentUseCase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetEnrollmentsByStudentUseCase(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<StudentClassResponse>> ExecuteAsync(Guid studentId, CancellationToken ct = default)
+        {
+            var enrollments = await _unitOfWork.Enrollments.GetActiveByStudentIdAsync(studentId, ct);
+            return enrollments.Select(EnrollmentMapper.ToStudentClassResponse);
+        }
+    }
+}
